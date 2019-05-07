9744771d68029589e41392291552367d43e8c2c8
Keenan Ganz <ganzkeenan1@gmail.com>
2019-05-07 17:49:50 -0400

    setup script removes database if it already exists

--- db_setup.py
+++ db_setup.py
@@ -7,15 +7,17 @@ connection = pymysql.connect(host="localhost",
 
 try:
     with connection.cursor() as cursor:
+        sql = "DROP DATABASE IF EXISTS crimemap"
+        cursor.execute(sql)
         sql = "CREATE DATABASE IF NOT EXISTS crimemap"
         cursor.execute(sql)
         sql = '''CREATE TABLE IF NOT EXISTS crimemap.crimes(
 id int NOT NULL AUTO_INCREMENT,
-latitude FLOAT(10, 6)
-longitude FLOAT(10, 6)
+latitude FLOAT(10, 6),
+longitude FLOAT(10, 6),
 date DATETIME,
-category VARCHAR(50)
-dscription VARCHAR(1000)
+category VARCHAR(50),
+dscription VARCHAR(1000),
 updated_at TIMESTAMP,
 PRIMARY KEY (id)
 )'''
